ba5cf54e8e7c94aedd15112a63046e9e80460797
Brandon Ray <brandon-ray.njinkeng@student.howest.be>
2026-03-17 12:52:24 +0100

    Add week05 LCD and servo work

--- lcd_test.py
+++ lcd_test.py
@@ -6,7 +6,7 @@ lcd.LCD_init()
 
 while True:
     lcd.LCD_string("Hello", lcd.LCD_LINE_1)
-    lcd.LCD_string("Brandon-Ray", lcd.LCD_LINE_2)git add .
+    lcd.LCD_string("Brandon-Ray", lcd.LCD_LINE_2)
     time.sleep(2)
 
     lcd.LCD_string("CTAI", lcd.LCD_LINE_1)
